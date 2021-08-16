7e7d8e94779ccad1defbbfe89a42b6cbb018a7ec
Vitaly Lavrov <lavrovvv@gmail.com>
2021-08-16 22:15:14 +0200

    Change title to ZaiChat

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,13 +7,13 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>ZaiChat</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
+            <IonTitle size="large">ZaiChat</IonTitle>
           </IonToolbar>
         </IonHeader>
         <ExploreContainer />
