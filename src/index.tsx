2e05ee839523e58cbcf37f6a59556d60572e8687
Vitaly Lavrov <lavrovvv@gmail.com>
2021-08-28 15:44:47 +0200

    Firebase support

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import App from './App';
 import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// https://firebase.google.com/docs/web/setup#available-libraries
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -20,3 +24,26 @@ serviceWorkerRegistration.unregister();
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+
+
+
+// Initialize Firebase
+
+const app = initializeApp({
+
+  apiKey: "AIzaSyB5Wz8s7NGkPA4AydXytGkJYNbhNXM1Jj0",
+
+  authDomain: "zaichat-lavrovvv.firebaseapp.com",
+
+  databaseURL: "https://zaichat-lavrovvv-default-rtdb.europe-west1.firebasedatabase.app",
+
+  projectId: "zaichat-lavrovvv",
+
+  storageBucket: "zaichat-lavrovvv.appspot.com",
+
+  messagingSenderId: "729286463893",
+
+  appId: "1:729286463893:web:c81588f5bdffc3b324269c"
+
+});
